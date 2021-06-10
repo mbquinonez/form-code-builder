bedc348185aab6b292b07d78d900cd4fa8815016
mbquinonez <mbquinonez@gmail.com>
2021-06-10 08:16:26 -0700

    PipeStarFormatBuilder
    Tool for creating |* codes

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,3 +1,4 @@
+import { registerLocaleData } from '@angular/common';
 import { Component } from '@angular/core';
 
 @Component({
@@ -6,5 +7,40 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'form-code-builder';
+ 
+  read: boolean;
+  write: boolean;
+  readwrite: string = '';
+  prefix: string = '';
+  columnCode: string = '';
+  elementCode: string = '';
+  useDateTime: boolean = false;
+  displayFormat:string = 'MMM dd, yyyy';
+
+  getOutputString() {
+    return `|* USEL:${this.getReadWrite()};ColumnCodes:${this.columnCode};Code:${this.prefix}${this.elementCode}${this.getSuffix()}`;
+  }
+
+  getSuffix() {
+    return this.useDateTime ? `;DataType:DateTime;DisplayFormat:${this.displayFormat}` : '';
+  }
+  getReadWrite() {
+    let retval = '';
+    if(this.read) retval += 'Read';
+    if(this.write) retval += 'Write';
+    return retval;
+  }
+  copyMessage(){
+    const selBox = document.createElement('textarea');
+    selBox.style.position = 'fixed';
+    selBox.style.left = '0';
+    selBox.style.top = '0';
+    selBox.style.opacity = '0';
+    selBox.value = this.getOutputString();
+    document.body.appendChild(selBox);
+    selBox.focus();
+    selBox.select();
+    document.execCommand('copy');
+    document.body.removeChild(selBox);
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,17 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-
+import { FormsModule } from '@angular/forms';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule,
+    BrowserAnimationsModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
